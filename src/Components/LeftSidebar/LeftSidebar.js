47111d94ba5d9a65a6d4e2d6f042c6219b7abfae
zzinue <zzinue@gmail.com>
2022-05-13 16:03:19 -0500

    adding sidebar icons

--- src/Components/LeftSidebar/LeftSidebar.js
+++ src/Components/LeftSidebar/LeftSidebar.js
@@ -1,9 +1,168 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    FcHome,
+    FcReading,
+    FcTodoList,
+    FcVideoCall,
+    FcAbout,
+    FcIdea,
+    FcShop,
+    FcLike,
+    FcBriefcase,
+    FcDisclaimer,
+    FcBusinessContact,
+    FcViewDetails,
+} from "react-icons/fc";
+import { AiFillAudio } from "react-icons/ai";
+import { FaTags, FaDev } from "react-icons/fa";
+import { IoLogoTwitter, IoLogoFacebook, IoLogoGithub, IoLogoInstagram, IoLogoTwitch } from "react-icons/io";
+import { RiInstagramFill, RiTwitchLine } from "react-icons/ri";
+import { CgShapeHexagon } from "react-icons/cg";
+import { GrFormClose } from "react-icons/gr";
+
 
 const LeftSidebar = () => {
-    return (
-        <div>LeftSidebar</div>
-    )
+    const [More, setMore] = useState(false)
+    const toggle = () => {
+        setMore(!More)
+    }
+    return <>
+        <aside className="LeftBar">
+            <nav className="LeftBar-Menu">
+                <ul>
+                    <li><a href="/Home"></a>
+                        <i>
+                            <FcHome />
+                        </i>
+                        Home
+                    </li>
+                    <li><a href="/Reading"></a>
+                        <i>
+                            <FcReading />
+                        </i>
+                        Reading List
+                    </li>
+                    <li><a href="/List"></a>
+                        <i>
+                            <FcTodoList />
+                        </i>
+                        List
+                    </li>
+                    <li><a href="/Podcast"></a>
+                        <i>
+                            <AiFillAudio />
+                        </i>
+                        Podcasts
+                    </li>
+                    <li><a href="/Videos"></a>
+                        <i>
+                            <FcVideoCall />
+                        </i>
+                        Videos
+                    </li>
+                    <li><a href="/Tags"></a>
+                        <i>
+                            <FaTags />
+                        </i>
+                        Tags
+                    </li>
+                    <li className={More ? "More Hidden" : 'More'}>
+                        <i></i>
+                        <a href="#" onClick={toggle}>More...</a>
+                    </li>
+                    <div className={More ? 'List' : "List Hidden"}>
+                        <li>
+                            <a href="/Code"></a>
+                            <i>
+                                <FcAbout />
+                            </i>
+                            Code of Conduct
+                        </li>
+                        <li>
+                            <a href="/FAQ"></a>
+                            <i>
+                                <FcViewDetails />
+                            </i>
+                            FAQ
+                        </li>
+                        <li><a href="/Dev">
+
+                            <i>
+                                <FcShop />
+                            </i>
+                            DEV Shop
+                        </a>
+                        </li>
+                        <li><a href="/Sponsors">
+
+                            <i>
+                                <FcLike />
+                            </i>
+                            Sponsors
+                        </a>
+                        </li>
+                        <li><a href="/About">
+                            <i>
+                                <FaDev />
+                            </i>
+                            About
+                        </a>
+                        </li>
+                        <li><a href="/Privacy">
+                            <i>
+                                <FcBriefcase />
+                            </i>
+                            Privacy Policy
+                        </a>
+                        </li>
+                        <li><a href="/Terms">
+                            <i>
+                                <FcDisclaimer />
+                            </i>
+                            Terms of Use
+                        </a>
+                        </li>
+                        <li><a href="/Contact">
+                            <i>
+                                <FcBusinessContact />
+                            </i>
+                            Contact
+                        </a>
+                        </li>
+
+                    </div>
+                </ul>
+                <div className={More ? 'LeftBar-Social' : 'LeftBar-Social Hidden '}>
+                    <a href="/Twitter">
+                        <i>
+                            <IoLogoTwitter />
+                        </i>
+                    </a>
+                    <a href="/Facebook">
+                        <i>
+                            <IoLogoFacebook />
+                        </i>
+                    </a>
+                    <a href="/Github">
+                        <i>
+                            <IoLogoGithub />
+                        </i>
+                    </a>
+                    <a href="/Instagram">
+                        <i>
+                            <RiInstagramFill />
+                        </i>
+                    </a>
+                    <a href="/Twitch">
+                        <i>
+                            <RiTwitchLine />
+                        </i>
+                    </a>
+
+                </div>
+            </nav>
+        </aside>
+    </>
 }
 
 export default LeftSidebar
